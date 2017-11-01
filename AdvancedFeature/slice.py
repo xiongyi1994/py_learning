c52711aa545f9e8aedb46ad692848a77a531e882
xiongyi <xiongyi940803@qq.com>
2017-11-01 15:53:39 +0800

    python中切片的使用方法

---
+++ AdvancedFeature/slice.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+list = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+# 取list的1到3个
+print(list[0:3])
+
+# 取list的倒数1到3个 倒数第一个元素的索引是-1
+print(list[-3:])
